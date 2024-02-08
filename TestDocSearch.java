1a8be077040ad312e41b122bf0e38d42aed1c242
Paz Mogrelia <paz.mogrelia@gmail.com>
2024-02-08 13:17:02 -0800

    completed lab tasks

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -20,5 +20,20 @@ public class TestDocSearch {
     String expect = String.format("Found 2 paths:\n.%stechnical%sbiomed%sar615.txt\n.%stechnical%splos%sjournal.pbio.0020150.txt", sep, sep, sep, sep, sep, sep);
     assertEquals(expect, h.handleRequest(rootPath));
 	}
+    @Test 
+	public void testSearch2() throws URISyntaxException, IOException {
+        Handler h = new Handler("./technical/");
+        URI rootPath = new URI("http://localhost/search?q=dhakshbnfilanifohaiodaklmdla");
+        assertEquals("Found 0 paths:\n", h.handleRequest(rootPath));
+	}
+    @Test
+    public void testSearch3() throws URISyntaxException, IOException {
+        Handler h = new Handler("./technical/");
+        String sep = File.separator;
+        URI rootPath = new URI("http://localhost/search?q=peanut");
+        String expect = String.format("Found 3 paths:\n.%stechnical%sbiomed%s1471-2229-3-3.txt\n.%stechnical%sbiomed%s1477-7827-1-43.txt\n.%stechnical%sgovernment%sEnv_Prot_Agen%stech_adden.txt", sep, sep, sep, sep, sep, sep,sep,sep,sep,sep,sep,sep,sep);
+        assertEquals(expect, h.handleRequest(rootPath));
+	}
+    
 }
 
